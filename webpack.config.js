a9f1db500f29efbb81f828b07d1e9ea7f0ee25fc
Geczy <Geczy@users.noreply.github.com>
2022-05-08 19:03:13 -0400

    add missing files

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  devtool: "inline-cheap-module-source-map",
+  devtool: 'inline-cheap-module-source-map',
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: "popup/build", to: "popup" },
-        { from: "chrome/icons", to: "icons" },
-        { from: "chrome/manifest.json", to: "manifest.json" },
-      ],
-    }),
+        { from: 'popup/build', to: 'popup' },
+        { from: 'chrome/icons', to: 'icons' },
+        { from: 'chrome/manifest.json', to: 'manifest.json' }
+      ]
+    })
   ],
   module: {
     rules: [
@@ -17,14 +17,14 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
+          loader: 'babel-loader'
+        }
+      }
+    ]
   },
   output: {
     chunkLoading: false,
-    wasmLoading: false,
+    wasmLoading: false
   },
-  target: ["web", "es5"],
+  target: ['web', 'es5']
 };
